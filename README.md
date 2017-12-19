4c81266a107f2193547623c4d7c93955d2424a46
davidkoojohn <davidkoojohn@gmail.com>
2017-12-19 16:53:58 +0800

    update index

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# davidkoojohn.github.io
-Personal website of John Kuo
+# [davidkoojohn](https://davidkoojohn.github.io)
+
+> Personal website of John Kuo
+
+*The website targets the latest version of Chrome, Safari, and Firefox.*
+
+## License
+
+MIT © [John Guo](https://davidkoojohn.github.io)
--- index.js
+++ index.js
@@ -15,62 +15,6 @@
 		document.head.appendChild(script);
 	}
 
-	// Latest blog post
-	(async () => {
-		const username = 'sindresorhus';
-		const graphql = `
-			query PostQuery($username: String!) {
-				posts(username: $username, limit: 1) {
-					title
-					url
-					subtitle
-				}
-			}
-		`;
-
-		const url = new URL('https://micro-medium-api-gqmwonovpk.now.sh/graphql');
-		url.searchParams.append('variables', JSON.stringify({username}));
-		url.searchParams.append('query', graphql);
-
-		const json = await (await fetch(url)).json();
-		const item = json.data.posts[0];
-
-		const title = dom.select('#latest-blog-post a');
-		title.href = item.url;
-		title.textContent = item.title;
-
-		dom.select('#latest-blog-post p').textContent = item.subtitle;
-		dom.select('#latest-blog-post').classList.add('visible');
-	})();
-
-	// Latest GitHub repos
-	(async () => {
-		const url = isDev ? 'http://localhost:3000' : 'https://gh-latest-repos-hbiycdtvej.now.sh';
-
-		const json = await (await fetch(url)).json();
-
-		const template = dom.select('#latest-repos-template');
-		const container = dom.select('#latest-repos');
-
-		for (const repo of json.reverse()) {
-			const content = template.cloneNode(true).content;
-
-			const a = content.querySelector('.latest-repos-title');
-			a.href = repo.url;
-			a.textContent = repo.name;
-
-			if (repo.primaryLanguage) {
-				const lang = content.querySelector('.latest-repos-language');
-				lang.textContent = repo.primaryLanguage.name;
-				lang.style.backgroundColor = repo.primaryLanguage.color;
-			}
-
-			content.querySelector('.latest-repos-description').textContent = repo.description;
-
-			container.appendChild(document.importNode(content, true));
-		}
-	})();
-
 	// Unicorn easteregg
 	dom.select('#unicorn-btn').addEventListener('click', event => {
 		event.preventDefault();
